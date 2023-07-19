35caf02c0aab9c5e5b31a5707c450b6abf747287
Martin Westerhof <a.m.westerhof@gmail.com>
2023-07-19 14:20:47 +0200

    deleting unused variables

--- src/controllers/weatherForecast.ts
+++ src/controllers/weatherForecast.ts
@@ -10,10 +10,6 @@ export const weatherForecast = async (req: Request, res: Response) => {
     try {
         const { longitude, latitude, days } = req.query;
 
-        const currentDate = new Date();
-        currentDate.setDate(currentDate.getDate() + 1);
-        const tomorrowDate = currentDate.toISOString().slice(0, 10);
-
         const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max,temperature_2m_min,windspeed_10m_max,winddirection_10m_dominant&timezone=auto&forecast_days=${days}`;
         const response = await axios.get(url);
         const data = response.data;
