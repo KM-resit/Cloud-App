1281f9259c2d2ae935126fc01de2c10a19c807f6
christo_stevan <christostevan2501@gmail.com>
2023-07-25 14:21:03 +0200

    Create get endpoint

--- src/index.ts
+++ src/index.ts
@@ -8,39 +8,7 @@ import { weatherHistory, weatherHistoryPerMonth } from './controllers/weatherHis
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
-
-const mssql = require('mssql');
-
-// For connection to the db
-const config = {
-    user: "userWeather",
-    password: "Admin123,!",
-    server: "kmresitserver.database.windows.net",
-    port: 1433,
-    database: "WeatherAppResit",
-    authentication: {
-        type: "default"
-    },
-    options: {
-        encrypt: true,
-    },
-};
-
-async function connectToDatabase() {
-    try {
-        const pool = await mssql.connect(config);
-        console.log("Reading rows from the table");
-        const result = await pool.request().query("Select * from [dbo].[User]");
-        console.log(result.recordset);
-
-        pool.close();
-        mssql.close();
-    } catch (err: any) {
-        console.error("Error connecting to the database: ", err.message);
-    }
-}
-
-// connectToDatabase();
+const {poolConnect, pool} = require("./database/db");
 
 const corsOptions = {
     origin: "*",
@@ -50,6 +18,7 @@ const corsOptions = {
 
   
 app.use(cors(corsOptions));
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, World!');
@@ -64,6 +33,18 @@ app.get('/currentWeather', currentWeather);
 app.get('/weatherForecast', weatherForecast); // will allow minimum forecast 1 day, max forecast 16 days
 app.get('/weatherHistory', weatherHistory);
 app.get('/weatherHistoryPerMonth', weatherHistoryPerMonth);
+app.get("/database", async (req, res) => {
+    try {
+        await poolConnect;
+        const request = pool.request();
+        const result = await request.query("SELECT * FROM [dbo].[User]");
+        res.json(result.recordset);
+    } catch (err: any) {
+        console.error("Error getting the data from database: ", err.message);
+        res.status(500).json({message: "Server error"});
+    }
+});
+
 
 const server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
